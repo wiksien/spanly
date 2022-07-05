2c9eecac5ca5916b7f5093c35627abaf554ec46f
wiksien <88692323+wiksien@users.noreply.github.com>
2022-07-05 16:47:39 +0200

    Added Spanly object with a function.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//Nothing, for now!
+function test(testInput) {
+  return true;
+}
+
+const Spanly = {
+  string: function (input, predicate, multipleSequencesCheck = false) {
+    console.log(input, predicate(input), multipleSequencesCheck);
+  },
+};
+
+Spanly.string("meow", test);
