f97348eb677cb6b0bf39a89f357987a6205baa1f
wiksien <88692323+wiksien@users.noreply.github.com>
2022-07-06 18:28:12 +0200

    Added Spanly functionality for arrays.

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function test(testInput) {
+function isEven(x) {
+  return Math.abs(x) % 2 === 0;
+}
+
+function testFunction(x) {
   return true;
 }
 
+//Spanly (object) -> key values for every input method -> individual function that does the job.
+
 const Spanly = {
-  string: function (input, predicate, multipleSequencesCheck = false) {
-    console.log(input, predicate(input), multipleSequencesCheck);
+  array: (arr, predicate, multipleSequencesCheck = false) => {
+    if (multipleSequencesCheck === true) {
+      let result = [];
+      let previousTrueInstance = 0;
+
+      for (let i = 0; i < arr.length; i++) {
+        if (predicate(arr[i])) {
+          result.push(arr.slice(previousTrueInstance, i + 1));
+          previousTrueInstance = i + 1;
+        }
+      }
+
+      if (result[0] === undefined) {
+        return arr;
+      } else {
+        if (previousTrueInstance !== arr.length) {
+          result.push(arr.slice(previousTrueInstance));
+        }
+        return result;
+      }
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        if (predicate(arr[i])) return [arr.slice(0, i + 1), arr.slice(i + 1)];
+      }
+
+      return arr;
+    }
   },
 };
 
-Spanly.string("meow", test);
+console.log(Spanly.array([3, 3, 3, 2, 3, 2], isEven, true));
